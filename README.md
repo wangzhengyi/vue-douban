5c4e5a3b698f4fdf547621515307410c4b1686ce
wangzhengyi <wzyll1314@gmail.com>
2017-04-25 17:49:48 +0800

    2017-04-25:完成底部导航栏

---
+++ README.md
@@ -0,0 +1,32 @@
+# douban
+
+> 小组作业，高仿豆瓣APP.参考链接:[vue.js2.0实战：仿豆瓣app项目](https://zhuanlan.zhihu.com/p/26522523)
+
+------
+## 项目依赖
+
+1. [Mint UI](http://mint-ui.github.io/#!/zh-cn)
+
+-----
+## 项目协作
+
+建议同学提PR，我来merge.
+
+------
+## Build Setup
+
+``` bash
+# install dependencies
+npm install
+
+# serve with hot reload at localhost:8080
+npm run dev
+
+# build for production with minification
+npm run build
+
+# build for production and view the bundle analyzer report
+npm run build --report
+```
+
+For detailed explanation on how things work, checkout the [guide](http://vuejs-templates.github.io/webpack/) and [docs for vue-loader](http://vuejs.github.io/vue-loader).
---
+++ src/pages/Index.vue
@@ -0,0 +1,66 @@
+<template>
+<div>
+  <mt-tabbar v-model="selected">
+    <mt-tab-item v-for="item in navList" :key="item.id" :id="item.id">
+      <img slot="icon" :src="tabImg(item)">
+      {{item.title}}
+    </mt-tab-item>
+  </mt-tabbar>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'Index',
+  data () {
+    return {
+      selected: 'home',
+      navList: [
+        {
+          id: 'home',
+          title: '首页',
+          imgNormal: require('../assets/images/ic_tab_home_normal.png'),
+          imgActive: require('../assets/images/ic_tab_home_active.png')
+        },
+        {
+          id: 'media',
+          title: '书影音',
+          imgNormal: require('../assets/images/ic_tab_subject_normal.png'),
+          imgActive: require('../assets/images/ic_tab_subject_active.png')
+        },
+        {
+          id: 'status',
+          title: '广播',
+          imgNormal: require('../assets/images/ic_tab_status_normal.png'),
+          imgActive: require('../assets/images/ic_tab_status_active.png')
+        },
+        {
+          id: 'group',
+          title: '小组',
+          imgNormal: require('../assets/images/ic_tab_group_normal.png'),
+          imgActive: require('../assets/images/ic_tab_group_active.png')
+        },
+        {
+          id: 'profile',
+          title: '我的',
+          imgNormal: require('../assets/images/ic_tab_profile_normal.png'),
+          imgActive: require('../assets/images/ic_tab_profile_active.png')
+        }
+      ]
+    }
+  },
+  methods: {
+    tabImg: function (item) {
+      console.log(item.id + ',' + this.selected)
+      if (item.id === this.selected) {
+        return item.imgActive
+      } else {
+        return item.imgNormal
+      }
+    }
+  }
+}
+</script>
+
+<style>
+</style>
